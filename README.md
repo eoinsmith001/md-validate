7786ca478c8c6ad6742530673333769c4e986f5d
esmith <eoinsmith001@gmail.com>
2016-04-07 22:57:50 +0100

    redis cache for auth api

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
-# Validation middleware example
+# Validation middleware example, with Redis Caching
 
 Hello Svc uses a middleware function which validates user exists in auth service before continuing to provide functionality
 
 ## Run 
 
+Assumes local availability of `mongod`, `nodemon`, `redis-server` (use `npm install -g`)
+
 ```
 mongod --dbpath ~/data
 cd auth
 nodemon server.js
+redis-server
 mocha populate/registerUser.js
 # Now there is one user (id=44)
 cd hello
@@ -15,3 +18,7 @@ nodemon server.js
 curl http://localhost:3001/api/hello/44 # => Ada says hello
 curl http://localhost:3001/api/hello/33 # => No user 33
 ```
+
+## Redis cache
+
+Auth Svc uses an artificially slow call to mongo to retrieve a user.  Subsequent calls for the same user are cached in the redisStore.
--- auth/routes/index.js
+++ auth/routes/index.js
@@ -1,25 +1,52 @@
-var express = require('express');
+var express      = require('express');
+var cacheManager = require('cache-manager');
+var redisStore   = require('cache-manager-redis');
+
 var User = require('../app/models/user');
 
 var router = express.Router();
 
+var ttl = 5;
+
+var redisCache = cacheManager.caching({
+  store: redisStore,
+  host: 'localhost',
+  port: 6379,
+  db: 0,
+  ttl: 600
+});
+
+redisCache.store.events.on('redisError', function(err) {
+  console.log(err.message);
+});
+
+function getUser(userid, cb) {
+  setTimeout(function() {
+    User.findOne({
+      id: userid
+    }, cb);
+  }, 5000);
+}
+
+
 router.route('/user/:userid')
 .get(function(req,res) {
   console.log('get', req.params);
-  User.findOne({
-    id: req.params.userid
-  }, function(err, user) {
+  var key = req.params.userid;
+  redisCache.wrap(key, function(cb) {
+    getUser(key, cb);
+  }, ttl, function(err, user) {
     if (err) {
       res.status(404).json({
         success: false,
         message: 'No such user '+req.params.userid
       });
     } else {
-      console.log('got', user);
-      res.status(200).json(user);
+     res.status(200).json(user);
     }
   });
 });
+
 router.route('/user')
 .post(function(req,res) {
   var user = new User({
